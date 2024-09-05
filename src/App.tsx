21a31e9e5b9de6b7165cdc0191a77b5c0fe0b787
kuancheng chi <a6232241pccu@gmail.com>
2024-09-05 17:46:15 +0800

    ADD: toggle dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [mode, setMode] = useState<"light" | "dark">();
+
+  const initialMode = (isDark: boolean) => {
+    setMode(isDark ? "dark" : "light");
+    localStorage.setItem("mode", isDark ? "dark" : "light");
+    if (isDark) document.body.classList.add("dark");
+    else document.body.classList.remove("dark");
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("mode")) {
+      initialMode(localStorage.getItem("mode") === "dark");
+    } else {
+      initialMode(window?.matchMedia("(prefers-color-scheme: dark)").matches);
+    }
+  }, []);
+
+  const toggleMode = () => {
+    setMode((prev) => (prev === "light" ? "dark" : "light"));
+    document.body.classList.toggle("dark");
+  };
+
   return (
-    <div className="App">
-      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"></button>
+    <div className={`h-screen w-screen bg-white dark:bg-slate-900`}>
+      <header className="flex gap-2">
+        <button className="text-black dark:text-white" onClick={toggleMode}>
+          {mode}
+        </button>
+      </header>
+      <p className="w-full overflow-hidden text-black md:w-8 dark:text-white">
+        Cyan
+      </p>
     </div>
   );
 }
